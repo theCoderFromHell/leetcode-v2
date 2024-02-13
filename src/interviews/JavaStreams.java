8ee22c7fe787da330c7a1e77a7c23b3301d76f8e
Aaquib Javed <thedevaaquib@gmail.com>
2024-02-13 20:07:57 +0530

    Java Streams Understanding

---
+++ src/interviews/JavaStreams.java
@@ -0,0 +1,173 @@
+package interviews;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class JavaStreams {
+
+    public static void main(String[] args) {
+        List<Student> studentList = Stream.of(
+                        new Student(1, "Rohit", 30, "Male", "Mechanical Engineering", "Mumbai", 122, Arrays.asList("+912632632782", "+1673434729929")),
+                        new Student(2, "Pulkit", 56, "Male", "Computer Engineering", "Delhi", 67, Arrays.asList("+912632632762", "+1673434723929")),
+                        new Student(3, "Ankit", 25, "Female", "Mechanical Engineering", "Kerala", 164, Arrays.asList("+912632633882", "+1673434709929")),
+                        new Student(4, "Satish Ray", 30, "Male", "Mechanical Engineering", "Kerala", 26, Arrays.asList("+9126325832782", "+1671434729929")),
+                        new Student(5, "Roshan", 23, "Male", "Biotech Engineering", "Mumbai", 12, Arrays.asList("+012632632782")),
+                        new Student(6, "Chetan", 24, "Male", "Mechanical Engineering", "Karnataka", 90, Arrays.asList("+9126254632782", "+16736784729929")),
+                        new Student(7, "Arun", 26, "Male", "Electronics Engineering", "Karnataka", 324, Arrays.asList("+912632632782", "+1671234729929")),
+                        new Student(8, "Nam", 31, "Male", "Computer Engineering", "Karnataka", 433, Arrays.asList("+9126326355782", "+1673434729929")),
+                        new Student(9, "Sonu", 27, "Female", "Computer Engineering", "Karnataka", 7, Arrays.asList("+9126398932782", "+16563434729929", "+5673434729929")),
+                        new Student(10, "Madarchod", 26, "Male", "Instrumentation Engineering", "Mumbai", 98, Arrays.asList("+912632646482", "+16734323229929")))
+                .toList();
+
+        List<Student> studentsByRank = studentList.stream()
+                .filter(s -> s.getRank() >= 50 && s.getRank() <= 100)
+                .toList();
+        System.out.println(studentsByRank);
+
+        List<Student> studentsFromKarnataka = studentList.stream()
+                .filter(student -> "Karnataka".equals(student.getCity()))
+                .sorted(Comparator.comparing(Student::getFirstName).reversed())
+                .toList();
+        System.out.println(studentsFromKarnataka);
+
+        List<String> departments = studentList.stream().map(Student::getDept).distinct().toList();
+        System.out.println(departments);
+
+        Set<String> departmentsInSet = studentList.stream().map(Student::getDept).collect(Collectors.toUnmodifiableSet());
+        System.out.println(departmentsInSet);
+
+        List<String> contacts = studentList.stream().flatMap(student -> student.getContacts().stream()).toList();
+        System.out.println(contacts);
+
+        List<String> list = studentList.stream().collect(Collectors.groupingBy(Student::getDept)).values().stream().flatMap(s -> s.stream().map(Student::getFirstName)).toList();
+        System.out.println(list);
+
+        Double averageMale = studentList.stream().filter(s -> "Male".equals(s.getGender())).collect(Collectors.averagingInt(Student::getAge));
+        System.out.println(averageMale);
+        Double averageFemale = studentList.stream().filter(s -> "Female".equals(s.getGender())).collect(Collectors.averagingInt(Student::getAge));
+        System.out.println(averageFemale);
+
+        Map<String, Double> averageAgeByGender = studentList.stream().collect(Collectors.groupingBy(Student::getGender, Collectors.averagingDouble(Student::getAge)));
+        System.out.println(averageAgeByGender);
+
+        Map<String, Optional<Student>> highestRanksByDept = studentList.stream().collect(Collectors.groupingBy(Student::getDept, Collectors.minBy(Comparator.comparingInt(Student::getRank))));
+        System.out.println(highestRanksByDept);
+
+        Optional<Student> skip = studentList.stream().sorted(Comparator.comparingInt(Student::getRank)).skip(3).findAny();
+        System.out.println(skip);
+    }
+}
+
+class Student {
+    private int id;
+    private String firstName;
+    private int age;
+    private String gender;
+    private String dept;
+    private String city;
+    private int rank;
+    private List<String> contacts;
+
+    public Student(int id, String firstName, int age, String gender, String dept, String city, int rank, List<String> contacts) {
+        this.id = id;
+        this.firstName = firstName;
+        this.age = age;
+        this.gender = gender;
+        this.dept = dept;
+        this.city = city;
+        this.rank = rank;
+        this.contacts = contacts;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getDept() {
+        return dept;
+    }
+
+    public void setDept(String dept) {
+        this.dept = dept;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public void setRank(int rank) {
+        this.rank = rank;
+    }
+
+    public List<String> getContacts() {
+        return contacts;
+    }
+
+    public void setContacts(List<String> contacts) {
+        this.contacts = contacts;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return (id == student.id && age == student.age && rank == student.rank && Objects.equals(firstName, student.firstName) && Objects.equals(gender, student.gender) && Objects.equals(dept, student.dept) && Objects.equals(city, student.city) && Objects.equals(contacts, student.contacts));
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, age, gender, dept, city, rank, contacts);
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", age=" + age +
+                ", gender='" + gender + '\'' +
+                ", dept='" + dept + '\'' +
+                ", city='" + city + '\'' +
+                ", rank=" + rank +
+                ", contacts=" + contacts +
+                '}';
+    }
+}
