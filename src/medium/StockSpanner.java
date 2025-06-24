aea1b8993055d80fa987903ed0b946acdd71b68e
Aaquib Javed (Macbook Pro 14) <thedevaaquib@gmail.com>
2025-06-24 22:46:29 +0530

    MinimumSubarrayKDistinctIntegers ApplySubstitutions StockSpanner

--- src/medium/StockSpanner.java
+++ src/medium/StockSpanner.java
@@ -1,36 +1,31 @@
 package medium;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Stack;
 
 public class StockSpanner {
-    Stack<Integer> stack;
-    List<Integer> stockPrice;
-
+    Stack<int[]> stack;
     public StockSpanner() {
         this.stack = new Stack<>();
-        this.stockPrice = new ArrayList<>();
     }
 
     public int next(int price) {
-        while (!stack.empty() && stockPrice.get(stack.peek()) < price) {
-            stack.pop();
+        int currStreak = 1;
+        while (!stack.empty() && stack.peek()[0] <= price) {
+            int[] top = stack.pop();
+            currStreak += top[1];
         }
-        int result = (stack.empty()) ? stockPrice.size() + 1 : stockPrice.size() - stack.peek();
-        stack.add(stockPrice.size());
-        stockPrice.add(price);
-        return result;
+        stack.add(new int[]{price, currStreak});
+        return currStreak;
     }
 
     public static void main(String[] args) {
-        StockSpanner s = new StockSpanner();
-        System.out.println(s.next(100));
-        System.out.println(s.next(80));
-        System.out.println(s.next(60));
-        System.out.println(s.next(70));
-        System.out.println(s.next(60));
-        System.out.println(s.next(75));
-        System.out.println(s.next(85));
+        StockSpanner S = new StockSpanner();
+        System.out.println(S.next(100));
+        System.out.println(S.next(80));
+        System.out.println(S.next(60));
+        System.out.println(S.next(70));
+        System.out.println(S.next(60));
+        System.out.println(S.next(75));
+        System.out.println(S.next(85));
     }
 }
