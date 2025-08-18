e07080ebd48c9e789b85971e001e6ce78deef69d
the Coder From Hell <74156786+theCoderFromHell@users.noreply.github.com>
2025-08-18 18:28:58 +0530

    FindThreeConsecutiveIntegersThatSumToAGivenNumber , MaximumConsecutiveFloorsWithoutSpecialFloors

---
+++ src/medium/FindThreeConsecutiveIntegersThatSumToAGivenNumber.java
@@ -0,0 +1,9 @@
+package medium;
+
+public class FindThreeConsecutiveIntegersThatSumToAGivenNumber {
+    public long[] sumOfThree(long num) {
+        if (num % 3 != 0)
+            return new long[]{};
+        return new long[]{num/3 -1, num/3, num/3 + 1};
+    }
+}
